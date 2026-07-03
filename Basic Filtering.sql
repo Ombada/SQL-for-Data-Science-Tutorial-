14d8125eb18c6108ff3070ba68dffcc5bf88f961
Mohamed Almna <98751963+Ombada@users.noreply.github.com>
2026-07-03 21:38:14 +0200

    Ch2

---
+++ Basic Filtering.sql
@@ -0,0 +1,55 @@
+-- Ch2: Basic Filtering 
+
+-- Filtering is associated with the word "WHERE"
+
+-- 1. Retrieve customers whose phone number is NULL
+SELECT *
+FROM customers
+WHERE phone IS NULL;
+
+
+-- 2. Retrieve customers living in CA or NY
+
+SELECT *
+FROM customers
+WHERE state IN ('CA', 'NY');
+
+-- Alternative
+
+SELECT *
+FROM customers
+WHERE state = 'CA' OR state = 'NY';
+
+
+-- 3. Retrieve customers whose first name starts with "A"
+
+SELECT *
+FROM customers
+WHERE first_name LIKE 'A%';
+
+
+-- 4. Retrieve customers whose last name contains "son"
+
+SELECT *
+FROM customers
+WHERE last_name LIKE '%son%';
+
+
+-- 5. Retrieve customers born between 1980 and 2000
+
+SELECT *
+FROM customers
+WHERE birth_date BETWEEN '1980-01-01' AND '2000-12-31';
+
+-- Alternative
+
+SELECT *
+FROM customers
+WHERE birth_date >= '1980-01-01' AND birth_date <= '2000-12-31';
+
+
+
+-- 6. Retrieve customers whose first name starts with B and who live in MA.
+SELECT *
+FROM customers
+WHERE first_name LIKE 'B%' AND state = 'MA';
