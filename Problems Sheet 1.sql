ef16ddfdb0f66b6d42cd81d54e837b5e99a066a6
Mohamed Almna <98751963+Ombada@users.noreply.github.com>
2026-07-04 12:00:10 +0200

    Add files via upload

---
+++ Problems Sheet 1.sql
@@ -0,0 +1,33 @@
+-- 1. Display all order items with the total_price
+SELECT order_id, product_id, quantity, unit_price,
+        (quantity*unit_price) as total_price
+FROM order_items;
+
+-- 2. Find the total value of each order.
+SELECT order_id, SUM(quantity * unit_price) AS order_total
+FROM order_items
+GROUP BY order_id;
+
+-- 3. Find the most expensive order
+SELECT order_id, SUM(quantity * unit_price) AS order_total
+FROM order_items
+GROUP BY order_id 
+order by order_total desc
+LIMIT 1;
+
+-- 4. Find the average order value
+SELECT  AVG(quantity * unit_price) AS order_avg
+FROM order_items;
+/* This is qite simple, however this its not correct 
+here is the correction you have to aggregate first then average */
+SELECT AVG(order_total) AS average_order_value
+FROM (
+    SELECT order_id,
+           SUM(quantity * unit_price) AS order_total
+    FROM order_items
+    GROUP BY order_id
+) t;
+
+-- 5.Find the total revenue generated
+SELECT SUM(quantity * unit_price) AS total_revenue
+FROM order_items;
